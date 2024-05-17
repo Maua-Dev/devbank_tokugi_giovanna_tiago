c6269543297a01534083e2fab7c1e9ededc4761b
gioalbuquerque <gio.oliver.albuquerque@gmail.com>
2024-05-17 16:20:37 -0300

    brinks do cliente

--- src/app/entities/item.py
+++ src/app/entities/item.py
@@ -8,13 +8,13 @@ class Item:
     price: float
     item_type: ItemTypeEnum
     admin_permission: bool = False
-    
+
     def __init__(self, name: str=None, price: float=None, item_type: ItemTypeEnum=None, admin_permission: bool=None):
         validation_name = self.validate_name(name)
         if validation_name[0] is False:
             raise ParamNotValidated("name", validation_name[1])
         self.name = name
-        
+
         validation_price = self.validate_price(price)
         if validation_price[0] is False:
             raise ParamNotValidated("price", validation_price[1])
@@ -24,12 +24,12 @@ class Item:
         if validation_item_type[0] is False:
             raise ParamNotValidated("item_type", validation_item_type[1])
         self.item_type = item_type
-        
+
         validation_admin_permission = self.validate_admin_permission(admin_permission)
         if validation_admin_permission[0] is False:
             raise ParamNotValidated("admin_permission", validation_admin_permission[1])
         self.admin_permission = admin_permission
-        
+
     @staticmethod
     def validate_name(name: str) -> Tuple[bool, str]:
         if name is None:
@@ -39,7 +39,7 @@ class Item:
         if len(name) < 3:
             return (False, "Name must be at least 3 characters long")
         return (True, "")
-        
+
     @staticmethod
     def validate_price(price: float) -> Tuple[bool, str]:
         if price is None:
@@ -49,7 +49,7 @@ class Item:
         if price < 0:
             return (False, "Price must be a positive number")
         return (True, "")
-    
+
     @staticmethod
     def validate_item_type(item_type: ItemTypeEnum) -> Tuple[bool, str]:
         if item_type is None:
@@ -57,7 +57,7 @@ class Item:
         if type(item_type) != ItemTypeEnum:
             return (False, "Item type must be a ItemTypeEnum")
         return (True, "")
-    
+
     @staticmethod
     def validate_admin_permission(admin_permission: bool) -> Tuple[bool, str]:
         if admin_permission is None:
@@ -65,7 +65,7 @@ class Item:
         if type(admin_permission) != bool:
             return (False, "Admin permission must be a boolean")
         return (True, "")
-        
+
     @staticmethod
     def validate_item_id(item_id: int) -> Tuple[bool, str]:
         if item_id is None:
@@ -73,13 +73,13 @@ class Item:
 
         if type(item_id) != int:
             return (False, "Parameter 'item_id' must be an integer")
-        
+
         if item_id < 0:
             return (False, "Parameter 'item_id' must be a positive integer")
 
         return (True, "")
-    
-        
+
+
     def to_dict(self):
         return {
             "name": self.name,
@@ -87,9 +87,9 @@ class Item:
             "item_type": self.item_type.value,
             "admin_permission": self.admin_permission
         }
-    
+
     def __eq__(self,other):
         return self.name == other.name and self.price == other.price and self.item_type == other.item_type and self.admin_permission == other.admin_permission
-    
+
     def __repr__(self):
         return f"Item(name={self.name}, price={self.price}, item_type={self.item_type}, admin_permission={self.admin_permission})"

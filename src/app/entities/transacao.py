748bee616a1ada84dcb50d646ab5063c3f43af36
Tiago Tokugi de Albuquerque Massuda <tiagomassuda123@gmail.com>
2024-05-17 14:09:46 -0300

    Validate do tipo e da quantia da entidade Transacao

--- src/app/entities/transacao.py
+++ src/app/entities/transacao.py
@@ -4,22 +4,44 @@ from ..errors.entity_errors import ParamNotValidated
 
 
 class Trasancao:
-    hora: str
-    quantia: int
+    hora: float
+    quantia: float
     tipo: TransacTypeEnum
-    saldoNaHora: int
+    saldoNaHora: float
 
-    def __init__(self, hora: str=None, quantia: int=None, tipo: TransacTypeEnum=None, saldoNaHora: int=None):
+    def __init__(self, hora: str = None, quantia: int = None, tipo: TransacTypeEnum = None, saldoNaHora: int = None):
         validate_quantia = self.validate_quantia(quantia, saldoNaHora)
         if validate_quantia[0] is False:
             raise ParamNotValidated("quantia", validate_quantia[1])
         self.quantia = quantia
 
+        validate_tipo = self.validate_tipo(tipo)
+        if validate_tipo[0] is False:
+            raise ParamNotValidated("tipo", validate_tipo[1])
+        self.tipo = tipo
 
     @staticmethod
     def validate_quantia(quantia: int, saldoNaHora: int) -> Tuple[bool, str]:
-        if quantia > saldoNaHora*2:
-            return (False, "Saldo Suspeito")
-        if quantia < 0:
-            return (False, "Quantia negativa não existe")
+        if quantia != int:
+            return (False, "A quantia deve ser um inteiro")
+        if quantia > saldoNaHora * 2:
+            return (False, "Saldo suspeito")
+        if quantia < 1:
+            return (False, "Quantia deve ser maior do que 1")
         return (True, "")
+
+    @staticmethod
+    def validate_tipo(tipo: TransacTypeEnum) -> Tuple[bool, str]:
+        if tipo is None:
+            return (False, "tipo de transação deve ser especificado")
+        if type(tipo) != TransacTypeEnum:
+            return (False, "O tipo deve existir em TransacTypeEnum")
+        return (True, "")
+
+    def to_dict(self):
+        return {
+            "type": self.tipo,
+            "current_balance": self.saldoNaHora,
+            "timestamp": self.hora,
+            "value": self.quantia
+        }

191ed1f82e0ea19876c6609f9a8b348dc5f50fe8
Tiago Tokugi de Albuquerque Massuda <tiagomassuda123@gmail.com>
2024-05-26 15:30:43 -0300

    correcao de erros 3

--- src/app/repo/transac_repositorio_mock.py
+++ src/app/repo/transac_repositorio_mock.py
@@ -1,20 +1,18 @@
 from typing import Dict, Optional
 
 from src.app.repo.transac_repositorio_interface import TransacRepository
-from ..entities .transacao import Trasancao
+from ..entities.transacao import Transacao
 
 
 class TransacRepositoriMock(TransacRepository):
-    transacoes:Dict[int,Transacao]
+    transacoes: Dict[int, Transacao]
 
     def __init__(self):
         self.transacoes = {}
 
-    def create_deposit(self, transac:Transacao, transac_id:int) -> Transacao:
+    def create_deposit(self, transac: Transacao, transac_id: int) -> Transacao:
         self.transacoes[transac_id] = transac
         return transac
 
-
-    def get_transac(self,transac_id:int) -> Optional[Trasancao]:
+    def get_transac(self, transac_id: int) -> Optional[Transacao]:
         return self.transacoes.get(transac_id, None)
-
